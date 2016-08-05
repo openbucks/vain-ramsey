90df91a41c25d44f7822219f97af547764ae0ebe
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-05 12:26:25 +0300

    Fixed typo in di.yml

---
+++ src/Extension/IdExtension.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-id
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-id
+ */
+namespace Vain\Id\Extension;
+
+use Vain\Core\Extension\AbstractExtension;
+
+/**
+ * Class IdExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class IdExtension extends AbstractExtension
+{
+}
